867b381f4aed2a5c28a89c6956aed796b0342843
Vishmithahash <vishmithahashendara@gmail.com>
2026-02-23 22:07:29 +0530

    Added file read feature

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+
 console.log("Hello Vishmitha Node.js LAB 04");
+
+
+fs.readFile('file.txt', 'utf8', function(err, data) {
+
+ 
+    if (err) {
+        console.log("Error reading file:", err);
+        return;
+    }
+
+    
+    console.log("File content:");
+    console.log(data);
+
+});
